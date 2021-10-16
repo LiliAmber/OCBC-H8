46582532934afc354c449605442b3776d0ca05c5
Rara Danniswara Musin <raradanniswaramusin@MacBook-Pro-Rara.local>
2021-10-16 20:21:45 +0700

    10th meeting

---
+++ sesi 10/TodoApp(default)/Controllers/TodoAppController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace TodoApp.Controllers
+{   
+    //=define the routing that our controller going to use
+    [Route("api/[controller]")]
+    //==need to specify the type of the controller to let .Net core know
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        //==define the routing for this section==
+        [Route("TestRun")]
+        [HttpGet]
+
+        public ActionResult TestTun() 
+        {
+            return Ok("Success");
+        }
+    }
+}
