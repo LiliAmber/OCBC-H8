b145b10aa552e4cb24703b15e3017b3c386ae0fd
Rara Danniswara Musin <raradanniswaramusin@MacBook-Pro-Rara.local>
2021-10-19 13:27:04 +0700

    unit testing done

---
+++ sesi 12/UnitTesting/EmployeeTest.cs
@@ -0,0 +1,41 @@
+using Moq;
+using System;
+using Xunit;
+using Unit_Test_Mock.Models;
+// using Unit_Test_Mock.Data;
+using Unit_Test_Mock.Services;
+using Unit_Test_Mock.Controllers;
+
+namespace UnitTesting
+{
+    public class EmployeeTest
+    {
+        #region Property
+        public Mock<IEmployeeService> mock = new Mock<IEmployeeService>();
+        #endregion
+
+        [Fact]
+        public async void GetEmployeebyId()
+        {
+            mock.Setup(p=> p.GetEmployeebyId(1)).ReturnsAsync("JK");
+            EmployeeController emp = new EmployeeController(mock.Object);
+            string result = await emp.GetEmployeeById(1);
+            Assert.Equal("JK", result);
+        }
+
+        [Fact]
+        public async void GetEmployeeDetails()
+        {
+            var employeeDTO = new Employee()
+            {
+                Id = 1,
+                Name = "JK",
+                Destination = "SDE"
+            };
+            mock.Setup(p=> p.GetEmployeeDetails(1)).ReturnsAsync(employeeDTO);
+            EmployeeController emp = new EmployeeController(mock.Object);
+            var result = await emp.GetEmployeeDetails(1);
+            Assert.True(employeeDTO.Equals(result));
+        }
+    }
+}
