62f025ece276e4e6deeb7e3c3c719ed5acf02481
LiliAmber <raradanniswaramusin@MacBook-Pro-Rara.local>
2021-11-08 11:56:14 +0700

    sesi1

---
+++ Python/sesi 1/sesi_1/app.py
@@ -0,0 +1,20 @@
+a = 2
+b = 3
+
+print(a ** 3)
+print('======')
+
+#pyton dictionary
+MLB_team = {    
+    'Colorado': 'Rockies',    
+    'Boston': 'Red Sox',    
+    'Minnesota': 'Twins',
+}
+print(MLB_team['Minnesota']) #akses value
+MLB_team['Boston'] = 'Red Fox'
+MLB_team['Boston'] = ['Red Fox', 'White Fox']
+MLB_team['Boston'].append('Grey Fox')
+
+#Adding an entry to an existing dictionary
+MLB_team['Kansas City'] = 'Royals'
+print(MLB_team)
